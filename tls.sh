3750d64897dcc851db4eb4014efb8ea642fca53a
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2022-09-17 00:00:24 +0200

    Fix creating self-signed certificates
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- tls.sh
+++ tls.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$SUBJECT" ]
 then
   echo "Environment variable SUBJECT must be exported or passed to the script."
@@ -8,14 +10,36 @@ fi
 
 SUBJECT_ALT_NAME=""
 
+# Create self-signed CA certificate
+openssl req \
+	-new -x509 -nodes -days 365 \
+	-subj "/CN=ca.${SUBJECT}" \
+	-newkey rsa:2048 \
+	-keyout root-ca.key \
+	-out root-ca.crt
+
 if [ -z "$SANS" ]
 then
-	openssl req \
-		-x509 -sha256 -nodes -days 3650 \
-		-newkey rsa:2048 \
+	# TODO: extract the commands below to a function and reuse it in the "else" block
+	# Create a private key
+	openssl genrsa 2048 > "${SUBJECT}".key
+	echo "done"
+	# Create a CSR
+	openssl req -new \
 		-subj "/CN=${SUBJECT}" \
-		-keyout "${SUBJECT}".key \
-		-out "${SUBJECT}".crt
+		-key "${SUBJECT}".key \
+		-out "${SUBJECT}".csr
+	echo "done"
+	# Sign the CSR with the self-signed CA certificate
+	openssl x509 -req -days 365 \
+		-in "${SUBJECT}".csr \
+		-out "${SUBJECT}".crt \
+		-CAcreateserial \
+		-CA root-ca.crt \
+		-CAkey root-ca.key
+	# cleanup temporary files
+	rm -f root-ca.*
+	rm -f "$SUBJECT".csr
 else
 	IFS=',' read -ra SANS_ARR <<< "$SANS"
 
